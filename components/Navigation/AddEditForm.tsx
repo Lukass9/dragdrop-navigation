e99e37610f64d3de45b55ef03b2d1a6a4fa772e4
Lukass9 <l_pisarek@o2.pl>
2024-11-29 06:38:32 +0100

    refactor(AddEditForm): update AddEditForm with integration for navigation flow

--- components/Navigation/AddEditForm.tsx
+++ components/Navigation/AddEditForm.tsx
@@ -1,13 +1,13 @@
-import { TrashIcon } from "@/components/icons";
-import { zodResolver } from "@hookform/resolvers/zod";
 import React from "react";
-import { SubmitHandler, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { FormSchema, formSchema } from "../../utils/validation";
+import { TrashIcon } from "../icons";
 
 interface AddEditFormProps {
   initialData?: Partial<FormSchema>;
   onSubmit: (data: FormSchema) => void;
-  onCancel?: () => void;
+  onCancel: () => void;
 }
 
 const AddEditForm: React.FC<AddEditFormProps> = ({
@@ -24,48 +24,31 @@ const AddEditForm: React.FC<AddEditFormProps> = ({
     defaultValues: initialData,
   });
 
-  const onSubmitForm: SubmitHandler<FormSchema> = (data) => {
-    onSubmit(data);
-  };
-
   return (
     <form
-      onSubmit={handleSubmit(onSubmitForm)}
+      onSubmit={handleSubmit(onSubmit)}
       className='border-border-primary border-2 rounded-lg flex flex-row text-center w-full bg-bg-primary'>
       <div className='flex flex-col text-left p-6 space-y-2 w-full'>
-        <div className='flex flex-col space-y-2'>
-          <label htmlFor='name' className='text-text-secondary-700'>
-            Nazwa
-          </label>
-          <input
-            {...register("name")}
-            type='text'
-            id='name'
-            placeholder='np. Promocje'
-            className='border-border-primary border-2 rounded-xl p-2'
-          />
-          {errors.name && (
-            <p className='text-red-500 text-sm'>{errors.name.message}</p>
-          )}
-        </div>
+        {(["label", "url"] as const).map((field) => (
+          <div key={field} className='flex flex-col space-y-2'>
+            <label className='text-text-secondary-700' htmlFor={field}>
+              {field === "label" ? "Nazwa" : "Link"}
+            </label>
+            <input
+              {...register(field)}
+              id={field}
+              className='border-border-primary border-2 rounded-xl p-2'
+              placeholder={
+                field === "label" ? "np. Promocje" : "Wklej lub wyszukaj"
+              }
+            />
+            {errors[field] && (
+              <p className='text-red-500'>{errors[field]?.message}</p>
+            )}
+          </div>
+        ))}
 
-        <div className='flex flex-col space-y-2'>
-          <label htmlFor='url' className='text-text-secondary-700'>
-            Link
-          </label>
-          <input
-            {...register("url")}
-            type='text'
-            id='url'
-            placeholder='Wklej lub wyszukaj'
-            className='border-border-primary border-2 rounded-xl p-2'
-          />
-          {errors.url && (
-            <p className='text-red-500 text-sm'>{errors.url.message}</p>
-          )}
-        </div>
-
-        <div className='flex flex-row space-x-2'>
+        <div className='flex space-x-2'>
           <button
             type='button'
             onClick={onCancel}
